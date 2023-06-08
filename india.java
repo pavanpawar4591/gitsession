76bf1c29e594ac43b823ad06afa829c0b56e630f
pavan.pawar <pavan.pawar@veritas.com>
2023-06-08 22:59:46 +0530

    new file add

--- india.java
+++ india.java
@@ -1,2 +1,19 @@
 India is my conuntry.and
 all india are my brother and sisters
+
+git checkout --no-track -b dev_ajay dev-pavan --
+
+git.exe checkout --no-track -b dev_akashay master --
+Switched to a new branch 'dev_akashay'
+
+
+git checkout -b dev_manish master
+
+
+
+------master --- mainline 
+
+v10.1
+	---
+git checkout -b dev_manali_10.1  v10.1
+
